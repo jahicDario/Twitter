c4731fa19b98e6bf8621c39ee926daa9b29c7da8
Dario <dariojahic95@gmail.com>
2016-03-23 23:20:04 +0100

    Uvedena JavaDoc dokumentacija za klasu TwitterPoruka za klasu, atribute i metode.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,32 +1,63 @@
 package com.twitter.poruke;
 
-
+/**
+ * 
+ * @author Dario
+ *Klasa koja sadrzi naziv korisnika i poruku koju korisnik prosledjuje.
+ */
 public class TwitterPoruka {
 
+	/**
+	 * Privatni atribut korisnik, koja predstavlja naziv korisnika koji ce proslediti poruku.
+	 */
 	private String korisnik;
+	/**
+	 *Privatni atribut poruka, koja ce sadrzi tekst koji ce korisnik proslediti.
+	 */
 	private String poruka;
-	
+	/**
+	 * Metoda koja vaca naziv korisnika
+	 * @return String - korisnik
+	 */
 	public String getKorisnik() {
 	return korisnik;
 	}
+	/**
+	 * Metoda koja kao parametar prima naziv korisnika i 
+	 * postavlja atribut na datu vrednost.
+	 * @param String - korisnik
+	 * @throws runtimeException - kada se kao parametar prosledi null ili prazan string.
+	 */
 	public void setKorisnik(String korisnik) {
 	if (korisnik==null || korisnik == "")
 	throw new RuntimeException(
 	"Ime korisnika mora biti uneto");
 	this.korisnik = korisnik;
 	}
-	
+	/**
+	 * Metoda koja vraca poruku.
+	 * @return String - poruka
+	 */
 	public String getPoruka() {
 	return "poruka";
 	}
-	
+	/**
+	 * Metoda koja kao parametar prima poruku i postavlja
+	 * atribut na datu vrednost.
+	 * @param String - poruka
+	 * @throws RuntimeException - kada se metodi prosledni poruka koja je jednaka null ili 
+	 * koja je duza od 140 karaktera.
+	 */
 	public void setPoruka(String poruka) {
 	if (poruka==null || this.poruka.length()>140)
 	throw new RuntimeException(
 	"Poruka mora biti uneta i mora imati najvise 140 znakova");
 	this.poruka = poruka;
 	}
-	
+	/**
+	 * Redefinisana toString metoda.
+	 * @return String - korsnik i poruka.
+	 */
 	public String toString(){
 	return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
 	}
